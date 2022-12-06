294a34b833bf295bdcc54b4d91b548f876987df1
Ville <ville777@proton.me>
2022-12-06 12:01:32 +0200

    graphics

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ class Game:
         self.screen = screen
         self.objects = []
         self.game_state = GameState.NONE
+        self.map = []
 
     def set_up(self):
         player = Player(1, 1)
@@ -17,11 +18,14 @@ class Game:
         print("do set up")
         self.game_state = GameState.RUNNING
 
+        self.load_map("01")
+
     def update(self):
         self.screen.fill(config.BLACK)
         print("update")
-
         self.handle_events()
+
+        self.render_map(self.screen)
 
         for object in self.objects:
             object.render(self.screen)
@@ -42,3 +46,28 @@ class Game:
                     self.player.update_position(-1, 0)
                 elif event.key == pygame.K_d:  # right
                     self.player.update_position(1, 0)
+
+    def load_map(self, file_name):
+        with open('maps/' + file_name + '.txt') as map_file:
+            for line in map_file:
+                tiles = []
+                for i in range(0, len(line) - 1, 2):
+                    tiles.append(line[i])
+                self.map.append(tiles)
+
+    def render_map(self, screen):
+        y_pos = 0
+        for line in self.map:
+            x_pos = 0
+            for tile in line:
+                image = map_tile_image[tile]
+                rect = pygame.Rect(x_pos * config.SCALE, y_pos * config.SCALE, config.SCALE, config.SCALE)
+                screen.blit(image, rect)
+                x_pos = x_pos + 1
+            y_pos = y_pos + 1
+
+
+map_tile_image = {
+    "G": pygame.transform.scale(pygame.image.load("imgs/grass1.png"), (config.SCALE, config.SCALE)),
+    "W": pygame.transform.scale(pygame.image.load("imgs/water.png"), (config.SCALE, config.SCALE))
+}
